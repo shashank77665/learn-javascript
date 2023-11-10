d2ea9a234eb24bed29b4895b24ded50f80eebf0c
shashank77665 <109029541+shashank77665@users.noreply.github.com>
2023-11-10 12:33:58 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+const name='Shashank'
+const UID='21BCS11754'
+//console.log('I am '+ name)
+//console.log(UID)
+//console.log(`I am ${name} and my UID is ${UID}` )
+
+const gameName= new String('Shashank-sk-com')
+
+console.log(gameName.charAt(3))
+
